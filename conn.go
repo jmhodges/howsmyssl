0aed35367247e9da68e5c9daacb2b2ce9c36e105
Jeff Hodges <jeff@somethingsimilar.com>
2013-10-27 02:46:31 -0700

    add checks for BEAST attack and unsupported cipher suites

--- conn.go
+++ conn.go
@@ -58,44 +58,44 @@ func (c *conn) Write(b []byte) (int, error) {
 // TODO(jmhodges): as this data gets larger, having the json encoding here
 // makes less sense.
 type tlsData struct {
-	CipherSuites            []string `json:"cipher_suites"`
-	EphemeralKeysSupported  bool     `json:"ephemeral_keys_supported"` // good
-	SessionTicketsSupported bool     `json:"session_ticket_supported"` // good
-	TLSCompressionSupported bool     `json:"tls_compression_support"`  // bad
+	CipherSuites                []string `json:"cipher_suites"`
+	EphemeralKeysSupported      bool     `json:"ephemeral_keys_supported"`       // good if true
+	SessionTicketsSupported     bool     `json:"session_ticket_supported"`       // good if true
+	TLSCompressionSupported     bool     `json:"tls_compression_supported"`      // bad if true
+	UnknownCipherSuiteSupported bool     `json:"unknown_cipher_suite_supported"` // bad if true
+	BEASTAttackVuln             bool     `json:"beast_attack_vuln"`              // bad if true
 }
 
 func (c *conn) TLSData() *tlsData {
-	ephemeralKeys := false
-	compressionSupported := false
-	cs := make([]string, 0)
+	d := &tlsData{}
 
 	c.handshakeMutex.Lock()
 	defer c.handshakeMutex.Unlock()
+
 	for _, ci := range c.st.ClientHello.CipherSuites {
 		s, found := allCipherSuites[ci]
 		if found {
 			if strings.Contains(s, "DHE_") {
-				ephemeralKeys = true
+				d.EphemeralKeysSupported = true
+			}
+			if c.st.ClientHello.Vers <= tls.VersionTLS10 && strings.Contains(s, "_CBC_") {
+				d.BEASTAttackVuln = true
 			}
 		} else {
+			d.UnknownCipherSuiteSupported = true
 			s = fmt.Sprintf("Some unknown cipher suite: %#x", ci)
 		}
-		cs = append(cs, s)
+		d.CipherSuites = append(d.CipherSuites, s)
 	}
-	ticketSupported := c.st.ClientHello.TicketSupported
+	d.SessionTicketsSupported = c.st.ClientHello.TicketSupported
 
 	for _, cm := range c.st.ClientHello.CompressionMethods {
 		if cm != 0x0 {
-			compressionSupported = true
+			d.TLSCompressionSupported = true
 			break
 		}
 	}
-	return &tlsData{
-		CipherSuites:            cs,
-		EphemeralKeysSupported:  ephemeralKeys,
-		SessionTicketsSupported: ticketSupported,
-		TLSCompressionSupported: compressionSupported,
-	}
+	return d
 }
 
 // This, unfortunately, means we take two uncontended locks on every read and
