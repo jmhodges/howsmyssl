053a1a773437a918b017321f87413bb042f95adf
Jeff Hodges <jeff@somethingsimilar.com>
2016-04-13 01:31:22 -0700

    check for PRs and force tags

--- travis_docker_push.sh
+++ travis_docker_push.sh
@@ -14,12 +14,19 @@ if [ "${TRAVIS_BRANCH}" != "master" ]; then
   exit
 fi
 
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  # TRAVIS_BRANCH is set to master on pull requests, so check if we're
+  # in a pull request.
+  echo "in a pull request, so no docker work needed"
+  exit
+fi
+
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS || die "unable to login"
 
 REPO=jmhodges/howsmyssl
 
 docker build -f Dockerfile -t $REPO .
-docker tag $REPO:$COMMIT $REPO:latest || die "unable to tag as latest"
-docker tag $REPO:$COMMIT $REPO:master-$TRAVIS_BUILD_NUMBER || die "unable to tag as master-$TRAVIS_BUILD_NUMBER"
+docker tag -f $REPO:$COMMIT $REPO:latest || die "unable to tag as latest"
+docker tag -f $REPO:$COMMIT $REPO:master-$TRAVIS_BUILD_NUMBER || die "unable to tag as master-$TRAVIS_BUILD_NUMBER"
 
 docker push $REPO || die "unable to push docker tags"
