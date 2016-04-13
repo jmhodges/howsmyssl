b5a2b9355503c79d89261eae8ed6f966517492cd
Jeff Hodges <jeff@somethingsimilar.com>
2016-04-13 00:53:52 -0700

    fix docker pushes (#77)
    * correct docker build and make sudo required
    
    * use the push

---
+++ travis_docker_push.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+
+function die() {
+  echo $1 > /dev/stderr
+  exit 1
+}
+
+if [ -z "${TRAVIS_BRANCH}"]; then
+  die "not running in travis"
+fi
+
+if [ "${TRAVIS_BRANCH}" != "master"]; then
+  echo "not on master, so no docker work needed"
+  exit
+fi
+
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS || die "unable to login"
+
+REPO=jmhodges/howsmyssl
+
+docker tag $REPO:$COMMIT $REPO:latest || die "unable to tag as latest"
+docker tag $REPO:$COMMIT $REPO:master-$TRAVIS_BUILD_NUMBER || die "unable to tag as master-$TRAVIS_BUILD_NUMBER"
+
+docker push $REPO || die "unable to push docker tags"
