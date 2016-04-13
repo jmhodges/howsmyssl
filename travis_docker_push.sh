105250e356bd6ac00e5f1f5e07cc7a043c602880
Jeff Hodges <jeff@somethingsimilar.com>
2016-04-13 01:34:45 -0700

    add .dockerignore and correct docker push script
    Hopefully, the last revision of this.

--- travis_docker_push.sh
+++ travis_docker_push.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-set -o errexit
-
 function die() {
   echo $1 > /dev/stderr
   exit 1
 }
 
-if [ -z "${TRAVIS_BRANCH}"]; then
+if [ -z "${TRAVIS_BRANCH}" ]; then
   die "not running in travis"
 fi
 
-if [ "${TRAVIS_BRANCH}" != "master"]; then
+if [ "${TRAVIS_BRANCH}" != "master" ]; then
   echo "not on master, so no docker work needed"
+  exit
+fi
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  # TRAVIS_BRANCH is set to master on pull requests, so check if we're
+  # in a pull request.
+  echo "in a pull request, so no docker work needed"
   exit
 fi
 
@@ -20,7 +25,8 @@ docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS || die "unable to 
 
 REPO=jmhodges/howsmyssl
 
-docker tag $REPO:$COMMIT $REPO:latest || die "unable to tag as latest"
-docker tag $REPO:$COMMIT $REPO:master-$TRAVIS_BUILD_NUMBER || die "unable to tag as master-$TRAVIS_BUILD_NUMBER"
+docker build -f Dockerfile -t $REPO .
+docker tag -f $REPO:$COMMIT $REPO:latest || die "unable to tag as latest"
+docker tag -f $REPO:$COMMIT $REPO:master-$TRAVIS_BUILD_NUMBER || die "unable to tag as master-$TRAVIS_BUILD_NUMBER"
 
 docker push $REPO || die "unable to push docker tags"
