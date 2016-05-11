44691c21ecd60aca099ea271540f50c7b3823c94
Jeff Hodges <jeff@somethingsimilar.com>
2016-05-10 17:10:12 -0700

    publish uptime stats and rename start_epoch_secs

--- howsmyssl.go
+++ howsmyssl.go
@@ -68,8 +68,14 @@ var (
 func main() {
 	flag.Parse()
 	t := time.Now()
-	expvar.NewInt("start_epoch_secs").Set(t.Unix())
-	expvar.NewString("start_timestamp").Set(t.Format(time.RFC3339))
+	expvar.NewInt("start_time_epoch_secs").Set(t.Unix())
+	expvar.NewString("start_time_timestamp").Set(t.Format(time.RFC3339))
+	expvar.Publish("uptime_secs", expvar.Func(func() interface{} {
+		return float64(time.Now().Sub(t) / time.Second)
+	}))
+	expvar.Publish("uptime_dur", expvar.Func(func() interface{} {
+		return time.Now().Sub(t)
+	}))
 
 	routeHost, redirectHost := calculateDomains(*rawVHost, *httpsAddr)
 
